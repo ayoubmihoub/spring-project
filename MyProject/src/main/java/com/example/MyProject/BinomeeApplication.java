061d32d7c7e40d0a61a72f71575d3205c29a144d
Lenovo <Lenovo@gmal.com>
2025-03-07 00:50:03 +0100

    first commit

---
+++ MyProject/src/main/java/com/example/MyProject/BinomeeApplication.java
@@ -0,0 +1,14 @@
+package com.example.MyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinomeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinomeeApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
